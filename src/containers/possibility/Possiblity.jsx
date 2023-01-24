547afa74015e8e9df58f24714576d748c83cc9cd
sagar27012001 <sagarpatil27012001@gmail.com>
2023-01-24 17:06:56 +0530

    React Tutorial

---
+++ src/containers/possibility/Possiblity.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './possibility.css';
+
+const Possiblity = () => {
+  return (
+    <div>Possiblity</div>
+  )
+}
+
+export default Possiblity;
--- src/environment.js
+++
@@ -1,3 +0,0 @@
-export const baseUrl = "http://localhost:5001";
-export const job = "/api/job/v1";
-export const restUriVacancy = "/vacancy/category";
